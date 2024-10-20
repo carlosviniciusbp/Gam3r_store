da84602193094b60e18d53fec047d8bfceed978d
carlosvbp <CarlosVinicius7345@gmail.com>
2024-10-20 08:57:31 -0300

    feat: added ListaProdutos component to the main page

--- frontend/src/app/(paginas)/page.tsx
+++ frontend/src/app/(paginas)/page.tsx
@@ -1,12 +1,9 @@
-import ProdutoItem from "@/components/produto/ProdutoItem"
-import { produtos } from "@/core"
+import ListaProdutos from "@/components/produto/ListaProdutos"
 
 export default function Home() {
   return (
-    <div className="container flex flex-col gap-5 py-10">
-      {produtos.map((produto) => (
-        <ProdutoItem key={produto.id} produto={produto} />
-      ))}
+    <div className="container flex flex-1 flex-col gap-5 py-10">
+      <ListaProdutos />
     </div>
   )
 }

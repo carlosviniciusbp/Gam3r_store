e2be433fea64b66b6251c0d0bc77f2386e2002e1
Carlos Vinícius Bressani Parreira <CarlosVinicius7345@gmail.com>
2024-10-24 11:49:28 -0300

    feat: core features added globally

--- gam3r.store/apps/frontend/src/core/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./constants"
-export * from "./produto"
-export * from "./parcelamento"
-export * from "./utils"
---
+++ gam3r.store/packages/core/src/index.ts
@@ -0,0 +1,4 @@
+export * from "../../../apps/frontend/src/core/constants"
+export * from "../../../apps/frontend/src/core/produto"
+export * from "../../../apps/frontend/src/core/parcelamento"
+export * from "../../../apps/frontend/src/core/utils"

8d2ca420a9614dca87a1b1986afc32db291880f5
Carlos Vinícius Bressani Parreira <CarlosVinicius7345@gmail.com>
2024-10-26 17:17:36 -0300

    feat: modules added in the backend

--- gam3r.store/apps/backend/src/app.module.ts
+++ gam3r.store/apps/backend/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { ProdutoModule } from './produto/produto.module';
+import { PedidoModule } from './pedido/pedido.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
+  imports: [ProdutoModule, PedidoModule, DbModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

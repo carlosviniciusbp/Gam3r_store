3a3464a4310156555086d65a60fbdc3bda84fac8
Carlos Vinícius Bressani Parreira <CarlosVinicius7345@gmail.com>
2024-11-09 10:00:33 -0300

    feat: added context to manage the product

---
+++ gam3r.store/apps/frontend/src/data/contexts/ContextoProdutos.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { createContext, useCallback, useEffect, useState } from "react"
+import { FiltrarProdutos, Produto } from "@gstore/core"
+import useAPI from "../hooks/useAPI"
+
+export interface ContextoProdutosProps {
+  produtos: Produto[]
+  pesquisa: string
+  // eslint-disable-next-line no-unused-vars
+  setPesquisa: (pesquisa: string) => void
+  // eslint-disable-next-line no-unused-vars
+  produtoPorId: (id: number) => Produto | null
+}
+
+const ContextoProdutos = createContext<ContextoProdutosProps>({} as any)
+
+export function ProvedorProdutos(props: any) {
+  const { httpGet } = useAPI()
+  const [pesquisa, setPesquisa] = useState<string>("")
+  const [produtos, setProdutos] = useState<Produto[]>([])
+
+  const carregarProdutos = useCallback(async () => {
+    const produtos = await httpGet("/produtos")
+    setProdutos(produtos ?? [])
+  }, [httpGet])
+
+  useEffect(() => {
+    carregarProdutos()
+  }, [carregarProdutos])
+
+  return (
+    <ContextoProdutos.Provider
+      value={{
+        pesquisa,
+        get produtos() {
+          if (!pesquisa) return produtos
+          return new FiltrarProdutos().executar(pesquisa, produtos)
+        },
+        setPesquisa,
+        produtoPorId: (id: number) =>
+          produtos.find((produto) => produto.id === id) ?? null,
+      }}
+    >
+      {props.children}
+    </ContextoProdutos.Provider>
+  )
+}
+
+export default ContextoProdutos
--- gam3r.store/packages/core/src/produto/index.ts
+++ gam3r.store/packages/core/src/produto/index.ts
@@ -1,5 +1,7 @@
-import Especificacoes from "./Especificacoes.interface"
-import Precificavel from "./Precificavel.interface"
-import Produto from "./Produto.interface"
+import Especificacoes from './Especificacoes.interface'
+import Precificavel from './Precificavel.interface'
+import Produto from './Produto.interface'
+import FiltrarProdutos from './FiltrarProdutos'
 
 export type { Especificacoes, Precificavel, Produto }
+export { FiltrarProdutos }
